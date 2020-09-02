4ef7cc23636e1e1b0ade431476a59c63ac606a01
NibinC <c.nibi123@gmail.com>
2020-09-02 13:16:32 -0400

    Added action mailer

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,15 +1,17 @@
 class AdminUser < ApplicationRecord
 	
 
-	has_secure_password
-
-	PASSWORD_FORMAT = /\A
+ has_secure_password
+ 
+ EMAIL_FORMAT =  /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+ PASSWORD_FORMAT = /\A
   (?=.{8,})          # Must contain 8 or more characters
   (?=.*\d)           # Must contain a digit
   (?=.*[a-z])        # Must contain a lower case character
   (?=.*[A-Z])        # Must contain an upper case character
   (?=.*[[:^alnum:]]) # Must contain a symbol
 /x
+
  validates :username, :presence=>true,
  					  :length=> {:maximum=>50},
  					  :uniqueness=>true
@@ -18,5 +20,8 @@ class AdminUser < ApplicationRecord
  					  :length=>{:maximum=>50},
  					  :format=>{:with=>PASSWORD_FORMAT}
  
-
+  validates :email, :presence=>true,
+				    :uniqueness=>true,
+				    :length=>{:maximum=>30},
+				    :format=>{:with=>EMAIL_FORMAT}	
 end
