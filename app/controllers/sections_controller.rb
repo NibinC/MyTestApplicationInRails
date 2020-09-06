895a8e6df3260b0afff743ccb96b292ed5c89577
NibinC <c.nibi123@gmail.com>
2020-09-06 16:32:21 -0400

    Added section and formated

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,28 +1,63 @@
 class SectionsController < ApplicationController
   before_action :confirm_logged_in
+  before_action :find_page
+
   def index
+    @section = @page.section.paginate(page: params[:page], per_page: 4).order("created_at asc")
+  
   end
 
   def show
-
+    @section = Section.find(params[:id])
   end
 
   def new
+    @section = Section.new(:page_id=>@page.id)
   end
   
   def create
+    @section = Section.new(section_params)
+    if(@section.save)
+      flash[:notice]="section created succusfully"
+      redirect_to(sections_path(:page_id=>@page.id))
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @section = Section.find(params[:id])
   end
   
   def update
+    @section = Section.find(params[:id])
+    if @section.update_attributes(section_params)
+      flash[:notice]="Section Updated succusfully"
+      redirect_to(sections_path(:page_id=>@page.id))
+    else
+      reder 'edit'
+    end
+
   end
 
   def delete
+    @section = Section.find(params[:id])
   end
   
   def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    redirect_to(sections_path(:page_id=>@page.id))
   end
+  
+  private
+  
+  def section_params
+    params.require(:section).permit(:page_id,:name,:position,:visible,:content_type,:content)
+  end
+  def find_page
+   @page = Page.find(params[:page_id])
+  end
+
 
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,10 @@ class Subject < ApplicationRecord
 has_many :page
 	
 	validates_presence_of :name
+
+	scope :visible, ->{where(:visible=>true)}
+	scope :invisible, ->{where(:visible=>false)}
+	scope :sorted, ->{order("position asc")}
+	scope :newest_first, ->{order("created_at desc")}
+	scope :search, lambda{|query| where(["name LIKE ?","%#{query}%"])}
 end
